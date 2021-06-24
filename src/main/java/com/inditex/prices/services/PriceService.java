0b31ba0b3632e401d2357d74c4d0418fd36d618f
ezequiroga <ae.quiroga.88@gmail.com>
2021-06-24 13:25:01 -0300

    Refactor para eliminar consdicion y utilizar streams

--- src/main/java/com/inditex/prices/services/PriceService.java
+++ src/main/java/com/inditex/prices/services/PriceService.java
@@ -7,6 +7,7 @@ import com.inditex.prices.utils.Utils;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,20 +22,18 @@ public class PriceService {
     private PriceRepository priceRepository;
 
     public Optional<PriceDTO> findPrice(Long date, int productId, int brandId) {
-        List<Price> prices = priceRepository.findPrice(date, productId, brandId);
-        if (prices.isEmpty()) {
-            return Optional.empty();
-        }
-        Price p = prices.get(0);
-        PriceDTO dto = new PriceDTO(
-                p.getProductId(),
-                p.getBrand().getId(),
-                p.getId(),
-                Utils.dateDBToDateApi(p.getStartDate()),
-                Utils.dateDBToDateApi(p.getEndDate()),
-                p.getPrice());
-                
-        return Optional.of(dto);
+        return priceRepository
+                .findPrice(date, productId, brandId)
+                .stream()
+                .findFirst()
+                .map(p -> new PriceDTO(
+                    p.getProductId(),
+                    p.getBrand().getId(),
+                    p.getId(),
+                    Utils.dateDBToDateApi(p.getStartDate()),
+                    Utils.dateDBToDateApi(p.getEndDate()),
+                    p.getPrice())
+                );
     }
 
 }
